f211162f7523af43d43fb458b5079ffd83b94a6b
SHANYUAN <shanyuan55@icloud.com>
2016-07-20 13:22:00 +0200

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name labelsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the labelsApp
+ */
+angular.module('labelsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name labelsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the labelsApp
+ */
+angular.module('labelsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
