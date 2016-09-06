0397c9a1e201fbf66674810899d4a07e79ee631f
SHANYUAN <shanyuan55@icloud.com>
2016-09-06 09:40:38 +0200

    directive für confirm buttons

---
+++ app/scripts/directives/confirmbutton.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name labelsApp.directive:confirmButton
+ * @description
+ * # confirmButton
+ */
+angular.module('labelsApp')
+  .directive('confirmButton', function () {
+    return {
+        templateUrl: "views/directives/confirm-button.html",
+        restrict: 'E',
+        scope: {
+            onConfirm: '&'  // passes on-confirm attribute to this scope
+        },
+        link: function postLink(scope, element, attrs) {
+            scope.showConfirm = false;
+        }
+    };
+  });
