dc3bc2902a3a92b36fe4f35dd977cb2b17681da5
SHANYUAN <shanyuan55@icloud.com>
2016-09-08 22:25:30 +0200

    function to remove prefLabel

--- app/scripts/directives/preflabelbox.js
+++ app/scripts/directives/preflabelbox.js
@@ -7,17 +7,19 @@
  * # smallBox
  */
 angular.module('labelsApp')
-  .directive('prefLabelBox', function (ngDialog) {
+  .directive('prefLabelBox', function ($routeParams, ngDialog, LabelService, AuthService) {
     return {
         templateUrl: "views/directives/prefLabel-box.html",
         restrict: 'E',
         scope: {
             data: "="
         },
-        link: function postLink(scope) {
-            scope.onClick = function() {
-                console.log("clicked!");
+        link: function postLink(scope, element) {
 
+            /**
+             * Opens a dialog with detailed information.
+             */
+            scope.openDialog = function() {
                 ngDialog.open({
                     template: "views/dialogs/small-box-prefLabel.html",
                     className: 'bigdialog',
@@ -26,7 +28,36 @@ angular.module('labelsApp')
                     disableAnimation: true,
                     scope: scope
                 });
+            };
 
+            /**
+             * Deletes the current prefLabel.
+             */
+            scope.onDeleteClick = function() {
+
+                // get current concept
+                LabelService.get({id: $routeParams.lID}, function(concept) {
+
+                    // remove current prefLabel
+                    concept.prefLabels = _.filter(concept.prefLabels, function(o) {
+                        return o.value !== scope.data.value && o.lang !== scope.data.value;
+                    });
+
+                    // send updated label to server
+                    var jsonObject = {
+                        item: concept,
+                        user: AuthService.getUser().name
+                    };
+
+                    LabelService.update({id: $routeParams.lID}, jsonObject, function() {
+                        // delete element from DOM
+                        element.remove();
+
+                    }, function(err) {
+                        console.log(err);
+                    });
+
+                });
 
             };
         }
