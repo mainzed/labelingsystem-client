5482c2199299067ed3b055e58b5182d4f29a0e09
SHANYUAN <shanyuan55@icloud.com>
2016-09-26 17:35:27 +0200

    added footer component

---
+++ app/scripts/components/shared/footer/footer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+* @ngdoc directive
+* @name labelsApp.directive:vocabBox
+* @description
+* # vocabBox
+*/
+angular.module('labelsApp')
+
+.component('lsFooter', {
+    bindings: {},
+    template: '<p id="footer">labeling system / v0.1 /     log out</p>',
+    controller: function () {
+
+    }
+});
