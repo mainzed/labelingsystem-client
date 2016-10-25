ef0e70e3a98b5780c6d4dc3dc2614526aedce2f0
SHANYUAN <shanyuan55@icloud.com>
2016-10-25 16:09:21 +0200

    refactored vocab result boxes

--- app/scripts/services/resourcesservice.js
+++ app/scripts/services/resourcesservice.js
@@ -9,15 +9,38 @@
  */
 angular.module('labelsApp')
   .service('ResourcesService', function ($http, ConfigService) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-    this.get = function(url, success, failure) {
 
+    var Resource = function(data) {
+        this.description = data.description;
+        this.group = data.group;;
+        this.label = data.label;
+        this.lang = data.lang;
+        this.broaderTerms = data.broaderTerms;
+        this.narrowerTerms = data.narrowerTerms;
+        this.quality = data.quality;
+        this.scheme = data.scheme;
+        this.type = data.type;
+        this.uri = data.uri;
+    };
+
+    Resource.prototype.hasBroader = function() {
+        return this.broaderTerms.length > 0;
+    };
+
+    Resource.prototype.hasNarrower = function() {
+        return this.narrowerTerms.length > 0;
+    };
+
+    Resource.prototype.hasMore = function() {
+        return this.description || this.hasBroader() || this.hasNarrower();
+    }
+
+    this.get = function(url, success, failure) {
         $http.get(ConfigService.api + "/resourceinfo?uri=" + encodeURI(url)).then(function(response) {
-            success(response.data);
+            success(new Resource(response.data));
         }, function(response) {
             failure(response);
         });
-
     };
 
 });
