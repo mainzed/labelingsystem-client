958cef1f66d0028cf9fec68a305cd96b0f551b5b
SHANYUAN <shanyuan55@icloud.com>
2016-11-04 11:15:11 +0100

    improved translation box

--- app/scripts/components/concept-detail/translation-box/translation-box.js
+++ app/scripts/components/concept-detail/translation-box/translation-box.js
@@ -18,11 +18,11 @@ angular.module("labelsApp")
          */
         this.openDialog = function() {
             if (ctrl.mode !== "viewer") {
+
+                ctrl.newValue = ctrl.data.value;
                 ctrl.dialog = ngDialog.open({
                     template: "scripts/components/concept-detail/translation-box/dialog.html",
                     className: 'bigdialog',
-                    showClose: false,
-                    closeByDocument: false,
                     disableAnimation: true,
                     scope: $scope
                 });
@@ -57,7 +57,7 @@ angular.module("labelsApp")
          * Updates the current prefLabel with a newer term.
          * @param {string} newValue - updated term text
          */
-        this.updateTranslation = function(newValue) {
+        this.updateTranslation = function() {
 
             // get current concept
             LabelService.get({id: $routeParams.lID}, function(concept) {
@@ -69,13 +69,13 @@ angular.module("labelsApp")
                 }));
 
                 concept.translations.splice(index, 1, {
-                    value: newValue,
+                    value: ctrl.newValue,
                     lang: ctrl.data.lang
                 });
 
                 concept.save(function success() {
                     // temporarily update current element
-                    ctrl.data.value = newValue;
+                    ctrl.data.value = ctrl.newValue;
                     ctrl.dialog.close();
 
                 }, function error(res) {
@@ -83,5 +83,12 @@ angular.module("labelsApp")
                 });
             });
         };
+
+         // save changes when dialog is closed
+        $rootScope.$on('ngDialog.closed', function (e, $dialog) {
+            if (ctrl.dialog.id === $dialog.attr('id')) {  // is the resource dialog
+                ctrl.updateTranslation();
+            }
+        });
     }]
 });
