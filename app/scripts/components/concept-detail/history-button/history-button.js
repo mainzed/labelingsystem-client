0b99307f6fc2e6df115be9e0f4c832f417aea5b7
MatthiasDfnr <matthias.dufner@hs-mainz.de>
2016-11-07 16:41:47 +0100

    css bug fixes

--- app/scripts/components/concept-detail/history-button/history-button.js
+++ app/scripts/components/concept-detail/history-button/history-button.js
@@ -4,7 +4,7 @@ angular.module("labelsApp")
     bindings: {
         revisions: "="
     },
-    template: "<span ng-click='$ctrl.openDialog()' class='icon-time'></span>",
+    template: "<span class='float smalltype'></span><span ng-click='$ctrl.openDialog()' class='icon-time'></span><a ng-click='$ctrl.openDialog()' class='float smalltype'>history</a>",
     controller: ["$scope", "ngDialog", function($scope, ngDialog) {
         var ctrl = this;
 
